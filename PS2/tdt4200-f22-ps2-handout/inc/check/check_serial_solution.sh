af32bb8fdbfbe8a6ff1ba69e15833b4486212722
Faunvik <faunvik@gmail.com>
2022-09-21 23:37:47 +0200

    oof

--- PS2/tdt4200-f22-ps2-handout/inc/check/check_serial_solution.sh
+++
@@ -1,8 +0,0 @@
-echo
-echo "Comparing serial solution with reference data"
-echo "-----------------------------------------------------------"
-echo "Grid size: 256, Max iteration: 5000, Snapshot frequency: 40"
-echo
-./serial 1>/dev/null
-./check/compare_solutions 256 data/00050.bin check/references/n256/00050.bin
-echo
