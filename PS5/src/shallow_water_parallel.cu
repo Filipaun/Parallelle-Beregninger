e301c503ef84668725f182508e0fa35201eed56e
Faunvik <faunvik@gmail.com>
2022-11-05 15:53:18 +0100

    Made advanced time step

--- PS5/src/shallow_water_parallel.cu
+++ PS5/src/shallow_water_parallel.cu
@@ -7,10 +7,15 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <math.h>
+
+// new cuda headers
 #include <cuda.h>
+#include <cooperative_groups.h>
 
 #include "../inc/argument_utils.h"
 
+// cooperative group namespace
+namespace cg = cooperative_groups;
 
 typedef int64_t int_t;
 typedef double real_t;
@@ -55,6 +60,7 @@ real_t
     *d_acceleration_x = NULL,
     *h_acceleration_y = NULL,
     *d_acceleration_y = NULL,
+
     dx,
     dt;
 
@@ -81,7 +87,10 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
     }
 }
 
-// the two time step kernels for the serial cuda implementation
+
+
+// the two time step kernels for the SERIAL cuda implementation
+
 void __global__ time_step_1_serial (real_t *velocity_x, real_t *velocity_y,
                     real_t *acceleration_x, real_t *acceleration_y,
                     real_t *mass_velocity_x_0, real_t *mass_velocity_y_0,
@@ -95,14 +104,39 @@ void __global__ time_step_2_serial (real_t *acceleration_x, real_t *acceleration
                     real_t dt, real_t dx, int_t N
 );
 
+// the two time step kernels for the PARALLEL cuda implementation
+void __global__ time_step_1_parallel (real_t *velocity_x, real_t *velocity_y,
+                    real_t *acceleration_x, real_t *acceleration_y,
+                    real_t *mass_velocity_x_0, real_t *mass_velocity_y_0,
+                    real_t *mass_velocity, real_t *mass_0,
+                    real_t dt, real_t dx, int_t N
+);
+void __global__ time_step_2_parallel (real_t *acceleration_x, real_t *acceleration_y,
+                    real_t *mass_velocity_x_0, real_t *mass_velocity_x_1,
+                    real_t *mass_velocity_y_0, real_t *mass_velocity_y_1,
+                    real_t *mass_velocity, real_t *mass_0, real_t *mass_1,
+                    real_t dt, real_t dx, int_t N
+);
+
+// single time step kernel for cooperative groups
+void __global__ time_step_coop (real_t *velocity_x, real_t *velocity_y,
+                    real_t *acceleration_x, real_t *acceleration_y,
+                    real_t *mass_velocity_x_0, real_t *mass_velocity_x_1,
+                    real_t *mass_velocity_y_0, real_t *mass_velocity_y_1,
+                    real_t *mass_velocity, real_t *mass_0, real_t *mass_1,
+                    real_t dt, real_t dx, int_t N
+);
+
 // TODO: Rewrite boundary_condition as a device function.
 void __device__ boundary_condition_serial ( real_t *domain_variable, int sign, int_t N );
+void __device__ boundary_condition_parallel (int x, int y, real_t *domain_variable, int sign, int_t N );
+
+
 void domain_init ( void );
 void domain_save ( int_t iteration );
 void domain_finalize ( void );
 
-// generate grid and blocks size for some N
-void get_dims(N,dim3* grid_dim, dim*3 block_dim );
+
 
 // Pthreads threaded domain save function
 void *domain_save_threaded ( void *iter );
@@ -116,12 +150,33 @@ swap ( real_t** t1, real_t** t2 )
 	*t2 = tmp;
 }
 
+// generate grid and blocks size for some N
+void get_dims( int_t N, dim3* grid_dim, dim3* block_dim )
+{
+    if ( N+2 <= 32)
+    {
+        grid_dim -> x = 1;
+        grid_dim -> y = 1;
+
+        block_dim -> x = N+2;
+        block_dim -> y = N+2;
+    }
+    else
+    {
+        grid_dim -> x = (N+2)/32 + ( (N+2) % 32 != 0);
+        grid_dim -> y = (N+2)/32 + ( (N+2) % 32 != 0);
+
+        block_dim -> x = 32;
+        block_dim -> y = 32;
+    }
+}
+
 
 int
 main ( int argc, char **argv )
 {
 
-    int is_parallel = 0;
+    int is_parallel = 2;
     OPTIONS *options = parse_args( argc, argv );
     if ( !options )
     {
@@ -139,22 +194,48 @@ main ( int argc, char **argv )
         grid_dim, 
         block_dim;
 
-    switch (is_parallel)
+    if(is_parallel == 1)
     {
-    case 1:
+        printf("############# \n");
+        printf("Running simple parallel \n");
+        printf("############# \n");
+
         get_dims(N,&grid_dim, &block_dim);
-        break;
-    
-    default:
-        break;
+        
     }
+    else if(is_parallel == 2)
+    {
+        printf("############# \n");
+        printf("Running advanced parallel \n");
+        printf("############# \n");
+
+        get_dims(N,&grid_dim, &block_dim);
+
+
+        int numBlocksPerSm = 0;
+        int dev = 0;
+        int numThreads = 1024;
+        cudaDeviceProp deviceProp;
+        cudaGetDeviceProperties(&deviceProp, dev);
+        cudaOccupancyMaxActiveBlocksPerMultiprocessor(&numBlocksPerSm, time_step_coop, numThreads, 0);
+
+        printf(" Max numblockpersm: %d   SM: %d,  \n", numBlocksPerSm, deviceProp.multiProcessorCount);
+    }
+    else
+    {
+        // ... 
+    }
+
+    printf("N: %ld Max iterations: %ld, snap freq.: %ld \n", N, max_iteration, snapshot_frequency);
+    printf("Grid.x %d Grid.y %d Block.x %d Block.y %d \n", grid_dim.x, grid_dim.y, block_dim.x, block_dim.y);
+    printf("Total number of blocks: %d \n", grid_dim.x*grid_dim.y );
 
 
     for ( int_t iteration = 0; iteration <= max_iteration; iteration++ )
     {
-        switch (is_parallel)
+
+        if(is_parallel == 0)
         {
-        case 0:
             time_step_1_serial<<<1,1>>>(	d_velocity_x, 		d_velocity_y,
                             d_acceleration_x, 	d_acceleration_y,
                             d_mass_velocity_x_0,   d_mass_velocity_y_0,	
@@ -169,10 +250,39 @@ main ( int argc, char **argv )
                             dt, dx, N
 	        );
             cudaDeviceSynchronize();
-            break;
-        
-        default:
-            break;
+        }
+
+        else if(is_parallel == 1)
+        {
+            time_step_1_parallel<<<grid_dim,block_dim>>>(	d_velocity_x, 		d_velocity_y,
+                            d_acceleration_x, 	d_acceleration_y,
+                            d_mass_velocity_x_0,   d_mass_velocity_y_0,	
+                            d_mass_velocity, 	d_mass_0, 
+                            dt, dx, N
+	        );
+            cudaDeviceSynchronize();
+            time_step_2_parallel<<<grid_dim,block_dim>>>( d_acceleration_x, 	d_acceleration_y,
+                            d_mass_velocity_x_0,	d_mass_velocity_x_1,
+                            d_mass_velocity_y_0,	d_mass_velocity_y_1,
+                            d_mass_velocity, 	d_mass_0, d_mass_1,
+                            dt, dx, N
+	        );
+            cudaDeviceSynchronize();
+        }
+        else if(is_parallel == 2)
+        {
+            void *kernel_args[] = {&d_velocity_x, &d_velocity_y,
+                            &d_acceleration_x, 	&d_acceleration_y,
+                            &d_mass_velocity_x_0,	&d_mass_velocity_x_1,
+                            &d_mass_velocity_y_0,	&d_mass_velocity_y_1,
+                            &d_mass_velocity, 	&d_mass_0, &d_mass_1,
+                            &dt, &dx, &N};
+            cudaError_t c_err = cudaLaunchCooperativeKernel( (void *) time_step_coop, grid_dim, block_dim, kernel_args);
+            cudaErrorCheck(c_err);
+        }
+        else
+        {
+            // ... 
         }
 
 
@@ -290,31 +400,31 @@ time_step_1_parallel (real_t *velocity_x, real_t *velocity_y,
         real_t dt, real_t dx, int_t N
 )
 {
-    boundary_condition_serial ( mass_0, 1, N );
-    boundary_condition_serial ( mass_velocity_x_0, -1, N );
-    boundary_condition_serial ( mass_velocity_y_0, -1, N );
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
 
-    for ( int_t y=1; y<=N; y++ )
-        for ( int_t x=1; x<=N; x++ )
-        {
-            U(y,x) = PNU(y,x) / PN(y,x);
-            V(y,x) = PNV(y,x) / PN(y,x);
-        }
+    boundary_condition_parallel ( x, y, mass_0, 1, N );
+    boundary_condition_parallel ( x, y, mass_velocity_x_0, -1, N );
+    boundary_condition_parallel ( x, y, mass_velocity_y_0, -1, N );
 
-    for ( int_t y=1; y<=N; y++ )
-        for ( int_t x=1; x<=N; x++ )
-        {
-            PNUV(y,x) = PN(y,x) * U(y,x) * V(y,x);
-        }
+    if(( x > 0 ) && (x < N+1) && (y > 0) && (y < N+1))
+    {
+        U(y,x) = PNU(y,x) / PN(y,x);
+        V(y,x) = PNV(y,x) / PN(y,x);
 
-    for ( int_t y=0; y<=N+1; y++ )
-        for ( int_t x=0; x<=N+1; x++ )
-        {
-            DU(y,x) = PN(y,x) * U(y,x) * U(y,x)
+        PNUV(y,x) = PN(y,x) * U(y,x) * V(y,x);
+    }
+
+    if((x < N+2) && (y < N+2))
+    {
+        DU(y,x) = PN(y,x) * U(y,x) * U(y,x)
                     + 0.5 * gravity * ( PN(y,x) * PN(y,x) / density );
-            DV(y,x) = PN(y,x) * V(y,x) * V(y,x)
+        DV(y,x) = PN(y,x) * V(y,x) * V(y,x)
                     + 0.5 * gravity * ( PN(y,x) * PN(y,x) / density );
-        }
+
+    }
+
+            
 }
 
 
@@ -326,32 +436,88 @@ time_step_2_parallel (real_t *acceleration_x, real_t *acceleration_y,
         real_t dt, real_t dx, int_t N
 )
 {
-    for ( int_t y=1; y<=N; y++ )
-        for ( int_t x=1; x<=N; x++ )
-        {
-            PNU_next(y,x) = 0.5*( PNU(y,x+1) + PNU(y,x-1) ) - dt*(
-                            ( DU(y,x+1) - DU(y,x-1) ) / (2*dx)
-                          + ( PNUV(y,x+1) - PNUV(y,x-1) ) / (2*dx)
-            );
-        }
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
 
-    for ( int_t y=1; y<=N; y++ )
-        for ( int_t x=1; x<=N; x++ )
-        {
-            PNV_next(y,x) = 0.5*( PNV(y+1,x) + PNV(y-1,x) ) - dt*(
-                            ( DV(y+1,x) - DV(y-1,x) ) / (2*dx)
-                          + ( PNUV(y+1,x) - PNUV(y-1,x) ) / (2*dx)
-            );
-        }
+    if(( x > 0 ) && (x < N+1) && (y > 0) && (y < N+1))
+    {
+        PNU_next(y,x) = 0.5*( PNU(y,x+1) + PNU(y,x-1) ) - dt*(
+                        ( DU(y,x+1) - DU(y,x-1) ) / (2*dx)
+                        + ( PNUV(y,x+1) - PNUV(y,x-1) ) / (2*dx)
+        );
 
-    for ( int_t y=1; y<=N; y++ )
-        for ( int_t x=1; x<=N; x++ )
-        {
-            PN_next(y,x) = 0.25*( PN(y,x+1) + PN(y,x-1) + PN(y+1,x) + PN(y-1,x) ) - dt*(
-                           ( PNU(y,x+1) - PNU(y,x-1) ) / (2*dx)
-                         + ( PNV(y+1,x) - PNV(y-1,x) ) / (2*dx)
-            );
-        }
+        PNV_next(y,x) = 0.5*( PNV(y+1,x) + PNV(y-1,x) ) - dt*(
+                        ( DV(y+1,x) - DV(y-1,x) ) / (2*dx)
+                        + ( PNUV(y+1,x) - PNUV(y-1,x) ) / (2*dx)
+        );
+
+
+        PN_next(y,x) = 0.25*( PN(y,x+1) + PN(y,x-1) + PN(y+1,x) + PN(y-1,x) ) - dt*(
+                        ( PNU(y,x+1) - PNU(y,x-1) ) / (2*dx)
+                        + ( PNV(y+1,x) - PNV(y-1,x) ) / (2*dx)
+        );
+    }
+}
+
+// single time step kernel for cooperative groups
+void __global__ 
+time_step_coop (real_t *velocity_x, real_t *velocity_y,
+            real_t *acceleration_x, real_t *acceleration_y,
+            real_t *mass_velocity_x_0, real_t *mass_velocity_x_1,
+            real_t *mass_velocity_y_0, real_t *mass_velocity_y_1,
+            real_t *mass_velocity, real_t *mass_0, real_t *mass_1,
+            real_t dt, real_t dx, int_t N
+)
+{   
+    // define the grid group o
+    cg::grid_group thread_group = cg::this_grid();
+
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    boundary_condition_parallel ( x, y, mass_0, 1, N );
+    boundary_condition_parallel ( x, y, mass_velocity_x_0, -1, N );
+    boundary_condition_parallel ( x, y, mass_velocity_y_0, -1, N );
+
+    if(( x > 0 ) && (x < N+1) && (y > 0) && (y < N+1))
+    {
+        U(y,x) = PNU(y,x) / PN(y,x);
+        V(y,x) = PNV(y,x) / PN(y,x);
+
+        PNUV(y,x) = PN(y,x) * U(y,x) * V(y,x);
+    }
+
+    if((x < N+2) && (y < N+2))
+    {
+        DU(y,x) = PN(y,x) * U(y,x) * U(y,x)
+                    + 0.5 * gravity * ( PN(y,x) * PN(y,x) / density );
+        DV(y,x) = PN(y,x) * V(y,x) * V(y,x)
+                    + 0.5 * gravity * ( PN(y,x) * PN(y,x) / density );
+
+    }
+
+    thread_group.sync();
+
+    if(( x > 0 ) && (x < N+1) && (y > 0) && (y < N+1))
+    {
+        PNU_next(y,x) = 0.5*( PNU(y,x+1) + PNU(y,x-1) ) - dt*(
+                        ( DU(y,x+1) - DU(y,x-1) ) / (2*dx)
+                        + ( PNUV(y,x+1) - PNUV(y,x-1) ) / (2*dx)
+        );
+
+        PNV_next(y,x) = 0.5*( PNV(y+1,x) + PNV(y-1,x) ) - dt*(
+                        ( DV(y+1,x) - DV(y-1,x) ) / (2*dx)
+                        + ( PNUV(y+1,x) - PNUV(y-1,x) ) / (2*dx)
+        );
+
+
+        PN_next(y,x) = 0.25*( PN(y,x+1) + PN(y,x-1) + PN(y+1,x) + PN(y-1,x) ) - dt*(
+                        ( PNU(y,x+1) - PNU(y,x-1) ) / (2*dx)
+                        + ( PNV(y+1,x) - PNV(y-1,x) ) / (2*dx)
+        );
+    }
+    thread_group.sync();
+
 }
 
 
@@ -360,6 +526,7 @@ void __device__
 boundary_condition_serial ( real_t *domain_variable, int sign, int_t N )
 {
     #define VAR(y,x) domain_variable[(y)*(N+2)+(x)]
+    
     VAR(   0, 0   ) = sign*VAR(   2, 2   );
     VAR( N+1, 0   ) = sign*VAR( N-1, 2   );
     VAR(   0, N+1 ) = sign*VAR(   2, N-1 );
@@ -369,6 +536,75 @@ boundary_condition_serial ( real_t *domain_variable, int sign, int_t N )
     for ( int_t y=1; y<=N; y++ ) VAR(   y, N+1 ) = sign*VAR(   y, N-1 );
     for ( int_t x=1; x<=N; x++ ) VAR(   0, x   ) = sign*VAR(   2, x   );
     for ( int_t x=1; x<=N; x++ ) VAR( N+1, x   ) = sign*VAR( N-1, x   );
+    #undef VAR
+}
+
+// TODO: Rewrite boundary_condition as a device function.
+void __device__
+boundary_condition_parallel (int x, int y, real_t *domain_variable, int sign, int_t N )
+{
+    #define VAR(y,x) domain_variable[(y)*(N+2)+(x)]
+
+    if( x == 0)
+    {
+        if ( y == N+1)
+        {
+            VAR( N+1, 0   ) = sign*VAR( N-1, 2   );
+        }
+        else if ( y < N+1)
+        {
+            VAR(   y, 0   ) = sign*VAR(   y, 2   );
+        }
+    }
+
+    else if( x == N+1)
+    {
+        if ( y == N+1)
+        {
+            VAR( N+1, N+1 ) = sign*VAR( N-1, N-1 );
+        }
+        else if ( y < N+1)
+        {
+            VAR(   y, N+1 ) = sign*VAR(   y, N-1 );
+        }
+        else
+        {
+            // Out of bounds
+        }
+    }
+    
+    else if( y == 0)
+    {   
+        if (x == 0)
+        {
+            VAR(   0, 0   ) = sign*VAR(   2, 2   );
+        }
+        else if (x <= N+1)
+        {
+            VAR(   0, x   ) = sign*VAR(   2, x   );
+        }
+        else
+        {
+            // Out of bounds
+        }
+    }
+
+    else if( y == N+1)
+    {
+        if (y == 0)
+        {
+            VAR(   0, N+1 ) = sign*VAR(   2, N-1 );
+        }
+        else if (x <= N+1)
+        {
+            VAR(   N+1, x   ) = sign*VAR(   N-1, x   );
+        }
+        else
+        {
+            // Out of bounds
+        }
+    }
+
     #undef VAR
 }
 
